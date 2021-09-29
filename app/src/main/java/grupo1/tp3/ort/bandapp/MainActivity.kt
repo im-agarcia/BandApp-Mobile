6aca10151c8fef1e2bade525002c92b6479122a9
im-agarcia <im.agarcia@outlook.com>
2021-09-29 12:46:07 -0300

    Project init

---
+++ app/src/main/java/grupo1/tp3/ort/bandapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package grupo1.tp3.ort.bandapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
